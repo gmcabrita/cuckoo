b68adeabf51bdd10ee655751f1052338846cfee1
Gonçalo Cabrita <_@gmcabrita.com>
2014-11-26 23:12:06 +0000

    Add a test for Cuckoo.new/3.

--- test/cuckoo_test.exs
+++ test/cuckoo_test.exs
@@ -1,4 +1,14 @@
 defmodule CuckooTest do
   use ExUnit.Case
 
+  test "create new cuckoo filter" do
+    cf = Cuckoo.new(100, 16)
+    struct = %Cuckoo.Filter{
+               buckets: {:array, 32, 0, {:array, 4, 0, 0, 10}, 100},
+               fingerprint_size: 16,
+               fingerprints_per_bucket: 4,
+               max_num_keys: 100
+           }
+    assert cf == struct
+  end
 end
