4d51c970d47b839e10436c17f42328a8a5f009a5
Gonçalo Cabrita <_@gmcabrita.com>
2014-11-27 17:18:26 +0000

    Expand Bucket implementation and test collection.

--- lib/bucket/bucket.ex
+++ lib/bucket/bucket.ex
@@ -3,11 +3,56 @@ defmodule Cuckoo.Bucket do
   This module implements a Bucket.
   """
 
-  @type t :: :array.array(pos_integer)
+  @type t :: Array.t
 
   @spec new(pos_integer) :: t
   def new(n) do
-    :array.new(n, [{ :fixed, true }, { :default, 0 }])
+    Array.new([n, :fixed])
+  end
+
+  @doc """
+  Sets the entry `index` to `element`.
+
+  Returns the updated bucket.
+  """
+  @spec set(t, non_neg_integer, pos_integer) :: t
+  def set(bucket, index, element) do
+    Array.set(bucket, index, element)
+  end
+
+  @doc """
+  Returns the element at the specified `index`.
+  """
+  @spec get(t, non_neg_integer) :: pos_integer
+  def get(bucket, index) do
+    Array.get(bucket, index)
+  end
+
+
+  @doc """
+  Checks if the bucket has any room left.
+
+  Returns `{ :ok, index }` if it finds an empty entry in the bucket,
+  otherwise returns `{ :err, :full }`.
+  """
+  @spec has_room?(t) :: { :ok, pos_integer } | { :err, :full }
+  def has_room?(bucket) do
+    index = Enum.find_index(bucket, fn (x) -> x == nil end)
+    unless index do
+      { :err, :full }
+    else
+      { :ok, index }
+    end
+  end
+
+  @doc """
+  Returns `true` if the bucket contains the element, otherwise returns `false`.
+
+  Alternatively you can use `element in bucket` instead of calling this function.
+  """
+  @spec contains?(t, pos_integer) :: boolean
+  def contains?(bucket, element) do
+    element in bucket
   end
 
 end
--- test/bucket/bucket_test.exs
+++ test/bucket/bucket_test.exs
@@ -1,7 +1,20 @@
 defmodule Cuckoo.BucketTest do
   use ExUnit.Case
 
-  test "create new bucket" do
-    assert Cuckoo.Bucket.new(4) == {:array, 4, 0, 0, 10}
+  setup do
+    { :ok, bucket: Cuckoo.Bucket.new(4) }
+  end
+
+  test "create new bucket", %{bucket: bucket} do
+    assert bucket == Array.new(4)
+  end
+
+  test "check if a bucket has room", %{bucket: bucket} do
+    assert Cuckoo.Bucket.has_room?(bucket) == { :ok, 0 }
+  end
+
+  test "check if a bucket contains an element after inserting", %{bucket: bucket} do
+    bucket = bucket |> Cuckoo.Bucket.set(0, 10)
+    assert 10 in bucket
   end
 end
