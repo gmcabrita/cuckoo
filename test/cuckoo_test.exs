0b0d4fe3281f127f9682212ac3f8674d2b70d7da
Gonçalo Cabrita <_@gmcabrita.com>
2014-11-28 15:46:56 +0000

    Test deletion and false deletions.

--- test/cuckoo_test.exs
+++ test/cuckoo_test.exs
@@ -6,7 +6,15 @@ defmodule CuckooTest do
     assert Cuckoo.contains?(cf, "hello")
   end
 
-  test "occupancy and false positives" do
+  test "delete" do
+    {:ok, cf} = Cuckoo.new(100, 16) |> Cuckoo.insert("hello")
+    assert Cuckoo.contains?(cf, "hello")
+
+    {:ok, cf} = Cuckoo.delete(cf, "hello")
+    assert !Cuckoo.contains?(cf, "hello")
+  end
+
+  test "occupancy, false positives and removal" do
     total_inserts = 100000
     cf = Cuckoo.new(total_inserts, 16, 4)
     {cf, inserts} = Enum.reduce(
@@ -40,5 +48,37 @@ defmodule CuckooTest do
     false_positive_rate = 100.0 * false_count / total_queries
 
     assert false_positive_rate <= 0.015
+
+    total_removes = div(total_inserts, 2)
+    {cf, removal_count} = Enum.reduce(
+      1..total_removes,
+      {cf, 0},
+      fn (x, {acc, removes}) ->
+        case Cuckoo.delete(acc, x) do
+          {:ok, cf} -> {cf, removes + 1}
+          {:err, :inexistent} -> assert false
+        end
+      end
+    )
+
+    assert removal_count == total_removes
+
+    false_removal_count =
+    Enum.reduce(
+      1..total_removes,
+      0,
+      fn(x, i) ->
+        query = Cuckoo.contains?(cf, x)
+
+        if query do
+          i + 1
+        else
+          i
+        end
+      end
+    )
+
+    false_removal_rate = 100.0 * false_removal_count / total_removes
+    assert false_removal_rate <= 0.01
   end
 end
